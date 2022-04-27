35d92d7a222df3ca8d7b02c2d6b240ea9cd3b3e9
Mike Mitchell <alchemistikk@gmail.com>
2022-04-27 12:31:34 -0400

    Add button event listener with user prompt callbacks

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const wrapper = document.querySelector(".wrapper");
 const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+  getBookDetails();
+  displayBooks();
+});
+
+function getBookDetails() {
+  const title = prompt("Enter book title.");
+  const author = prompt("Enter book author.");
+  addBookToLibrary(title, author);
+}
 
 let myLibrary = [];
 
@@ -14,6 +24,7 @@ function addBookToLibrary(title, author) {
 }
 
 function displayBooks() {
+  wrapper.replaceChildren();
   myLibrary.forEach(function (book) {
     const div = document.createElement("div");
     const p = document.createElement("p");
